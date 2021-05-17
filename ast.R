4b93b877a7c7b0a6bfb91d2ddd95dd2b1b11e893
Jay Morgan <jaymiles17@gmail.com>
2021-05-17 21:28:44 +0100

    Add vector data type

--- ast.R
+++ ast.R
@@ -9,7 +9,7 @@ ast <- function(ui) {
   }
 
   tokenize <- function(ui) {
-    str_regex <- "(?:(\\(|\\))|([\"*+\\/\\w\\d-]+))"
+    str_regex <- "(?:(\\(|\\))|([\\[\\]#{}])|([\"*+\\w\\d\\/-]+))"
     tokens <- stringr::str_match_all(ui, str_regex)[[1]][,1]
     return(tokens)
   }
@@ -18,13 +18,28 @@ ast <- function(ui) {
     return(paste0(arg1, op, arg2))
   }
 
+  to_vector <- function(tokens) {
+    v <- c("c(")
+    start <- FALSE
+    for (token in tokens) {
+      if (token == "[") { start <- TRUE; next }
+      if (token != "]" && start == TRUE) {
+        v <- c(v, token, ",")
+      }
+      if (token == "]" && start == TRUE) {
+        break
+      }
+    }
+    v <- v[1:(length(v)-1)]
+    v <- c(v, ")")
+    v <- paste0(v, collapse="")
+    return(v)
+  }
+
   general_fun <- function(...) {
     args <- list(...)
     func <- args[[1]]
     args <- args[2:length(args)][[1]]
-    ## for (i in 1:length(args)) {
-    ##   args[[i]] <- str2lang(args[[i]])
-    ## }
     fun <- paste0(func, "(", paste0(args, collapse = ", "), ")")
     return(fun)
   }
@@ -32,6 +47,25 @@ ast <- function(ui) {
   compile_function <- function(inputs) {
     func <- inputs[[1]]
     args <- inputs[2:length(inputs)]
+
+    if ("[" %in% args) {
+      # find the start and end point of the vector
+      start_idx <- which("[" == args)
+      end_idx <- which("]" == args)
+      v <- to_vector(args[start_idx:end_idx])
+      new_args <- c()
+
+      # resize arguments concatenating middle vector
+      if (start_idx != 1) {
+        new_args <- c(new_args, args[1:(start_idx-1)])
+      }
+      new_args <- c(new_args, v)
+      if (end_idx != length(args)) {
+        new_args <- c(new_args, args[1:(end_idx)])
+      }
+      args <- new_args
+    }
+
     if (is_infix(func)) {
       fun <- arithmetic_fun(func, args[[1]], args[[2]])
     } else {
@@ -41,12 +75,19 @@ ast <- function(ui) {
   }
 
   eval_function <- function(fun) {
-    out <- eval(parse(text=fun))
+    tryCatch({
+        out <- eval(parse(text=fun))
+    },
+    error=function(cond) {
+      message(paste0(cond, "\n"))
+      return(NA)
+    })
   }
 
   tokens <- tokenize(ui)
   end_point <- -1
   start_point <- -1
+  output <- ""
 
   for (i in length(tokens):1) {
     t <- tokens[[i]]
