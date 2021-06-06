80ac61ff63ae1a7d4a7de39cf8c08bbee3bf5685
Jay Morgan <jaymiles17@gmail.com>
2021-06-06 22:50:47 +0100

    Add %>% as a threading-macro like

--- R/ast.R
+++ R/ast.R
@@ -28,7 +28,7 @@ ast <- function(input) {
   }
 
   fun_re <- "^\\("
-  fun_con <- "(?:(\\(|\\))|([\\[\\]#{}])|(\".*?\")|([*+<>^:?=&$||\\w\\d\\/.-]+))"
+  fun_con <- "(?:(\\(|\\))|([\\[\\]#{}])|(\".*?\")|([*+<>^:?=&%$||\\w\\d\\/.-]+))"
 
   is_fun <- function(input) {
     return(stringr::str_detect(input, fun_re))
